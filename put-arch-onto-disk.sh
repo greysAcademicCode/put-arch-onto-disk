dd2f4bb299dc0c0e39dade47cec1b6e698647298
l3iggs <l3iggs@live.com>
2015-05-11 16:45:05 +0200

    add uefi boot support

--- put-arch-onto-disk.sh
+++ put-arch-onto-disk.sh
@@ -5,10 +5,10 @@ set -vx #echo on
 : ${ROOT_FS_TYPE:=f2fs}
 : ${MAKE_SWAP_PARTITION:=false}
 : ${SWAP_SIZE_IS_RAM_SIZE:=false}
-: ${SWAP_SIZE:=1GiB}
-: ${USE_TARGET_DISK:=false}
+: ${SWAP_SIZE:=100MiB}
+: ${GET_SIZE_FROM_TARGET:=false}
 : ${TARGET_DISK:=/dev/sdX}
-: ${IMG_SIZE:=3GiB}
+: ${IMG_SIZE:=2GiB}
 : ${IMG_NAME:=bootable_arch.img}
 : ${TIME_ZONE:=Europe/Copenhagen}
 : ${LANGUAGE:=en_US}
@@ -24,9 +24,10 @@ set -vx #echo on
 : ${CLEAN_UP:=false}
 : ${ENABLE_AUR:=true}
 : ${TARGET_IS_REMOVABLE:=false}
+: ${SUPPORT_UEFI:=false}
 
 rm -f "${IMG_NAME}"
-if [ "$USE_TARGET_DISK" = true ] ; then
+if [ "$GET_SIZE_FROM_TARGET" = true ] ; then
   DISK_INFO=$(lsblk -n -b -o SIZE,PHY-SEC ${TARGET_DISK})
   IFS=' ' read -a DISK_INFO_A <<< "$DISK_INFO"
   IMG_SIZE=$(numfmt --to-unit=K ${DISK_INFO_A[0]})KiB
@@ -34,42 +35,42 @@ if [ "$USE_TARGET_DISK" = true ] ; then
 fi
 fallocate -l $IMG_SIZE "${IMG_NAME}"
 wipefs -a -f "${IMG_NAME}"
-sgdisk -n 0:+0:+1MiB -t 0:ef02 -c 0:biosGrub "${IMG_NAME}"
-sgdisk -n 0:+0:+200MiB -t 0:8300 -c 0:ext4Boot "${IMG_NAME}"
-NEXT_PARTITION=3
+
+NEXT_PARTITION=1
+[ "$SUPPORT_UEFI" = false ] && sgdisk -n 0:+0:+1MiB -t 0:ef02 -c 0:biosGrub "${IMG_NAME}" && ((NEXT_PARTITION++))
+sgdisk -n 0:+0:+512MiB -t 0:ef00 -c 0:boot "${IMG_NAME}"; BOOT_PARTITION=$NEXT_PARTITION; ((NEXT_PARTITION++))
 if [ "$MAKE_SWAP_PARTITION" = true ] ; then
   if [ "$SWAP_SIZE_IS_RAM_SIZE" = true ] ; then
     SWAP_SIZE=`free -b | grep Mem: | awk '{print $2}' | numfmt --to-unit=K`KiB
   fi
-  sgdisk -n 0:+0:+${SWAP_SIZE} -t 0:8200 -c 0:swap "${IMG_NAME}"
-  NEXT_PARTITION=4
+  sgdisk -n 0:+0:+${SWAP_SIZE} -t 0:8200 -c 0:swap "${IMG_NAME}"; SWAP_PARTITION=$NEXT_PARTITION; ((NEXT_PARTITION++))
 fi
-#sgdisk -N 0 -t 0:8300 -c 0:${ROOT_FS_TYPE}Root "${IMG_NAME}"
-sgdisk -N ${NEXT_PARTITION} -t ${NEXT_PARTITION}:8300 -c ${NEXT_PARTITION}:${ROOT_FS_TYPE}Root "${IMG_NAME}"
+#sgdisk -N 0 -t 0:8300 -c 0:${ROOT_FS_TYPE}Root "${IMG_NAME}"; ROOT_PARTITION=$NEXT_PARTITION; ((NEXT_PARTITION++))
+sgdisk -N ${NEXT_PARTITION} -t ${NEXT_PARTITION}:8300 -c ${NEXT_PARTITION}:${ROOT_FS_TYPE}Root "${IMG_NAME}"; ROOT_PARTITION=$NEXT_PARTITION; ((NEXT_PARTITION++))
 
 LOOPDEV=$(sudo losetup --find)
 sudo losetup -P ${LOOPDEV} "${IMG_NAME}"
-sudo wipefs -a -f ${LOOPDEV}p2
-sudo mkfs.ext4 -L ext4Boot ${LOOPDEV}p2
-sudo wipefs -a -f ${LOOPDEV}p3
+sudo wipefs -a -f ${LOOPDEV}p${BOOT_PARTITION}
+sudo mkfs.fat -F32 -n BOOT ${LOOPDEV}p${BOOT_PARTITION}
 if [ "$MAKE_SWAP_PARTITION" = true ] ; then
-  sudo mkswap -L swap ${LOOPDEV}p3
-  sudo wipefs -a -f ${LOOPDEV}p4
+  sudo wipefs -a -f ${LOOPDEV}p${SWAP_PARTITION}
+  sudo mkswap -L swap ${LOOPDEV}p${SWAP_PARTITION}
 fi
+sudo wipefs -a -f ${LOOPDEV}p${ROOT_PARTITION}
 ELL=L
 [ "$ROOT_FS_TYPE" = "f2fs" ] && ELL=l
-sudo mkfs.${ROOT_FS_TYPE} -${ELL} ${ROOT_FS_TYPE}Root ${LOOPDEV}p${NEXT_PARTITION}
+sudo mkfs.${ROOT_FS_TYPE} -${ELL} ${ROOT_FS_TYPE}Root ${LOOPDEV}p${ROOT_PARTITION}
 sgdisk -p "${IMG_NAME}"
 TMP_ROOT=/tmp/diskRootTarget
 mkdir -p ${TMP_ROOT}
-sudo mount -t${ROOT_FS_TYPE} ${LOOPDEV}p${NEXT_PARTITION} ${TMP_ROOT}
+sudo mount -t${ROOT_FS_TYPE} ${LOOPDEV}p${ROOT_PARTITION} ${TMP_ROOT}
 sudo mkdir ${TMP_ROOT}/boot
-sudo mount -text4 ${LOOPDEV}p2 ${TMP_ROOT}/boot
-sudo pacstrap ${TMP_ROOT} base grub ${PACKAGE_LIST}
+sudo mount ${LOOPDEV}p${BOOT_PARTITION} ${TMP_ROOT}/boot
+sudo pacstrap ${TMP_ROOT} base grub btrfs-progs dosfstools exfat-utils f2fs-tools gpart parted jfsutils mtools nilfs-utils ntfs-3g hfsprogs ${PACKAGE_LIST}
 sudo sh -c "genfstab -U ${TMP_ROOT} >> ${TMP_ROOT}/etc/fstab"
 sudo sed -i '/swap/d' ${TMP_ROOT}/etc/fstab
 if [ "$MAKE_SWAP_PARTITION" = true ] ; then
-  SWAP_UUID=$(lsblk -n -b -o UUID ${LOOPDEV}p3)
+  SWAP_UUID=$(lsblk -n -b -o UUID ${LOOPDEV}p${SWAP_PARTITION})
   sudo sed -i '$a #swap' ${TMP_ROOT}/etc/fstab
   sudo sed -i '$a UUID='${SWAP_UUID}'	none      	swap      	defaults  	0 0' ${TMP_ROOT}/etc/fstab
 fi
@@ -93,12 +94,6 @@ if [ "$MAKE_ADMIN_USER" = true ] ; then
   sed -i 's/# %wheel ALL=(ALL) NOPASSWD: ALL/## %wheel ALL=(ALL) NOPASSWD: ALL/g' /etc/sudoers
   sed -i 's/# %wheel ALL=(ALL)/%wheel ALL=(ALL)/g' /etc/sudoers
 fi
-if [ "$ROOT_FS_TYPE" = "f2fs" ] ; then
-  pacman -S --needed --noconfirm f2fs-tools
-fi
-if [ "$ROOT_FS_TYPE" = "btrfs" ] ; then
-  pacman -S --needed --noconfirm btrfs-progs
-fi
 if [ "$ENABLE_AUR" = true ] ; then
   echo "[archlinuxfr]" >> /etc/pacman.conf
   echo "SigLevel = Never" >> /etc/pacman.conf
@@ -109,7 +104,7 @@ if [ "$ENABLE_AUR" = true ] ; then
   sed -i '$ d' /etc/pacman.conf
   pacman -Sy
 fi
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/GRUB_CMDLINE_LINUX_DEFAULT="/g' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet/GRUB_CMDLINE_LINUX_DEFAULT="rootwait/g' /etc/default/grub
 INSTALLED_PACKAGES=\$(pacman -Qe)
 if [[ \$INSTALLED_PACKAGES == *"openssh"* ]] ; then
   systemctl enable sshd.service
@@ -118,16 +113,43 @@ if [[ \$INSTALLED_PACKAGES == *"networkmanager"* ]] ; then
   systemctl enable NetworkManager.service
 fi
 if [[ \$INSTALLED_PACKAGES == *"bcache-tools"* ]] ; then
-  sed -i 's/MODULES=""/MODULES="bcache"/g' /etc/mkinitcpio.conf
+  sed -i 's/MODULES="/MODULES="bcache /g' /etc/mkinitcpio.conf
   sed -i 's/HOOKS="base udev autodetect modconf block/HOOKS="base udev autodetect modconf block bcache/g' /etc/mkinitcpio.conf
 fi
 mkinitcpio -p linux
-grub-install --modules=part_gpt --target=i386-pc --recheck --debug ${LOOPDEV}
 grub-mkconfig -o /boot/grub/grub.cfg
-
+pacman -Sy --needed --noconfirm os-prober
+#grub-mkconfig -o /boot/EFI/BOOT/grub.cfg
 if [ "$ROOT_FS_TYPE" = "f2fs" ] ; then
-  ROOT_UUID=$(lsblk -n -b -o UUID ${LOOPDEV}p${NEXT_PARTITION})
-  sed -i 's,root=${LOOPDEV}p${NEXT_PARTITION},root=UUID='\$ROOT_UUID',g' /boot/grub/grub.cfg
+  cat > /usr/sbin/fix-f2fs-grub.sh <<END
+#!/usr/bin/env bash
+ROOT_DEVICE=\\\$(df | grep -w / | awk {'print \\\$1'})
+ROOT_UUID=\\\$(blkid -s UUID -o value \\\${ROOT_DEVICE})
+sed -i 's,root=/[^ ]* ,root=UUID='\\\${ROOT_UUID}' ,g' \\\$1
+END
+  chmod +x /usr/sbin/fix-f2fs-grub.sh
+  fix-f2fs-grub.sh /boot/grub/grub.cfg
+fi
+if [ "$SUPPORT_UEFI" == true ] ; then
+  pacman -S --needed --noconfirm efibootmgr
+  #grub-install --modules="part_gpt fat linux gzio all_video" --removable --target=x86_64-efi --efi-directory=/boot --recheck --debug > /boot/grub-install.log
+  #echo 'configfile ${cmdpath}/grub.cfg' > /tmp/grub.cfg
+  mkdir -p /boot/EFI/BOOT
+  grub-mkstandalone -d /usr/lib/grub/x86_64-efi/ -O x86_64-efi --modules="part_gpt part_msdos" --fonts="unicode" --locales="en@quot" --themes="" -o "/boot/EFI/BOOT/BOOTX64.EFI" /boot/grub/grub.cfg=/boot/grub/grub.cfg  -v
+cat > /usr/sbin/fix-efi.sh <<END
+#!/usr/bin/env bash
+if [ efivar --list > /dev/null ] ; then
+  grub-install --removable --target=x86_64-efi --efi-directory=/boot --recheck --debug
+  grub-mkconfig -o /boot/grub/grub.cfg
+  if [ "$ROOT_FS_TYPE" = "f2fs" ] ; then
+    fix-f2fs-grub.sh /boot/grub/grub.cfg
+  fi
+fi
+END
+chmod +x /usr/sbin/fix-efi.sh
+else
+  grub-install --modules=part_gpt --target=i386-pc --recheck --debug ${LOOPDEV}
+  #grub-mkconfig -o /boot/grub/grub.cfg
 fi
 EOF
 if [ "$DD_TO_TARGET" = true ] ; then
@@ -139,13 +161,10 @@ sudo mv /tmp/chroot.sh ${TMP_ROOT}/root/chroot.sh
 sudo arch-chroot ${TMP_ROOT} /root/chroot.sh
 sudo rm ${TMP_ROOT}/root/chroot.sh
 
-sync
-sudo umount ${TMP_ROOT}/boot
-sudo umount ${TMP_ROOT}
-sudo losetup -D
-sync
+sync && sudo umount ${TMP_ROOT}/boot && sudo umount ${TMP_ROOT} && sudo losetup -D && sync && echo "Image sucessfully created"
 if [ "$DD_TO_TARGET" = true ] ; then
-  sudo -E bash -c 'dd if='"${IMG_NAME}"' of='${TARGET_DISK}' bs=1M; sync; sgdisk -e '${TARGET_DISK}'; sgdisk -v '${TARGET_DISK}'; [ '"$TARGET_IS_REMOVABLE"' = true ] && eject '${TARGET_DISK}
+  echo "Writing image to disk..."
+  sudo -E bash -c 'dd if='"${IMG_NAME}"' of='${TARGET_DISK}' bs=4M && sync && sgdisk -e '${TARGET_DISK}' && sgdisk -v '${TARGET_DISK}' && [ '"$TARGET_IS_REMOVABLE"' = true ] && eject '${TARGET_DISK} && echo "Image sucessfully written. It's now safe to remove ${TARGET_DISK}"
 fi
 
 if [ "$CLEAN_UP" = true ] ; then
